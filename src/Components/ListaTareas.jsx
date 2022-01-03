ad168f6b1c9fd1c190e54a6a78c456eb5d63a1f8
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-01-02 19:59:40 -0600

    Se añadio el css cuando una tarea este completada

--- src/Components/ListaTareas.jsx
+++ src/Components/ListaTareas.jsx
@@ -5,13 +5,13 @@ import '../css/ListaTareas.css'
 function ListaTareas() {
     return(
         <section className='lista_tareas'>
-            <Tarea tarea="Hacer el quehacer" />
-            <Tarea tarea="Terminar serie de Netflix"  />
-            <Tarea tarea="Estudiar"  />
-            <Tarea tarea="Lavar platos"  />
-            <Tarea tarea="Terminar serie de Netflix"  />
-            <Tarea tarea="Estudiar para mi examen de programacion funcional de SAP"  />
-            <Tarea tarea="Lavar platos"  />
+            <Tarea tarea="Hacer el quehacer" completed={false} />
+            <Tarea tarea="Terminar serie de Netflix" completed={true} />
+            <Tarea tarea="Estudiar" completed={false}  />
+            <Tarea tarea="Lavar platos" completed={false}  />
+            <Tarea tarea="Terminar serie de Netflix" completed={false} />
+            <Tarea tarea="Estudiar para mi examen de programacion funcional de SAP" completed={true} />
+            <Tarea tarea="Lavar platos" completed={false} />
         </section>
     )
 }
--- src/Components/Tarea.jsx
+++ src/Components/Tarea.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import '../css/Tarea.css'
 
-function Tarea({ tarea }) {
+function Tarea({ tarea, completed }) {
     return(
         <div className='tarea'>
             <span className='tarea__completar'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-check" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#6f32be" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" className={!completed ? 'icon icon-tabler icon-tabler-check' : 'icon icon-tabler icon-tabler-check completed__tick'} width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#6f32be" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M5 12l5 5l10 -10" />
                 </svg>
             </span>
-            <p className='tarea__descripcion'>{tarea}</p>
+            <p className={!completed ? 'tarea__descripcion' : 'tarea__descripcion completed__text'}>{tarea}</p>
             <button type='button' className='tarea__borrar'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#6f32be" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
