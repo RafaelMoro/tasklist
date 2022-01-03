37082139f672b046b2d37075b26dba1cb1a64f95
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-01-03 01:05:26 -0600

    Se termino la configuracion del hook taskContext, los componentes ya consumen la info de localstorage

--- src/Components/ListaTareas.jsx
+++ src/Components/ListaTareas.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import Tarea from './Tarea'
 import '../css/ListaTareas.css'
 
-function ListaTareas() {
+function ListaTareas({tareas}) {
     return(
         <section className='lista_tareas'>
-            <Tarea tarea="Hacer el quehacer" completed={false} />
-            <Tarea tarea="Terminar serie de Netflix" completed={true} />
-            <Tarea tarea="Estudiar" completed={false}  />
-            <Tarea tarea="Lavar platos" completed={false}  />
-            <Tarea tarea="Terminar serie de Netflix" completed={false} />
-            <Tarea tarea="Estudiar para mi examen de programacion funcional de SAP" completed={true} />
-            <Tarea tarea="Lavar platos" completed={false} />
+            {tareas.map( (tarea) => (
+                <Tarea key={tarea.id} tarea={tarea.nombre} completed={tarea.completed} />
+                    )
+                )
+            }
         </section>
     )
 }
