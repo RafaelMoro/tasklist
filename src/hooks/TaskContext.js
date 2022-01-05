a71f56a9257d01137764665e1cf17edcaa00410e
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-01-05 01:51:06 -0600

    Se arreglo bug crasheo cuando no hay datos

--- src/hooks/TaskContext.js
+++ src/hooks/TaskContext.js
@@ -5,18 +5,23 @@ const Context = React.createContext()
 function TaskContext(props) {
     //Our state that saves our list of tasks
     const [tasks, setTasks] = React.useState([])
-    //Gets the new version of the localStorage file
-    const tasklist = JSON.parse(localStorage.getItem('TASKLIST_V1'))
-
-    //If our tasks array is empty, fill it with the information from the local Storage
-    if(tasks.length < 1) {
-        setTasks(tasklist)
-    }
     //A state for filtered tasks depending on the input search bar
     const [filteredTasks, setFilteredTasks] = React.useState([])
-    
-    if (filteredTasks.length < 1) {
-        setFilteredTasks(tasks)
+    //Gets the new version of the localStorage file
+    let getStorageInfo = JSON.parse(localStorage.getItem('TASKLIST_V1'))
+
+    //If our tasks array is empty, fill it with the information from the local Storage
+    if((!getStorageInfo)||(getStorageInfo.length < 1)) {
+        localStorage.setItem('TASKLIST_V1', JSON.stringify(tasks))
+    }else {
+        if(tasks.length < 1) {
+            setTasks(getStorageInfo)
+        }
+    }
+    if(filteredTasks.length < 1) {
+        if(tasks.length >= 1) {
+            setFilteredTasks(tasks)
+        }
     }
     return (
         <Context.Provider value={{
